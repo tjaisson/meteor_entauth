b0ab08817c5ef543664bf8f4fd58b7cf46ec3c8f
tjaisson <thomas.jaisson@ac-paris.fr>
2020-06-30 18:16:12 +0200

    cypher : a new iv (initialization vector) each time

--- accounts-entcore-multi/cypher.js
+++ accounts-entcore-multi/cypher.js
@@ -16,12 +16,12 @@ EntcoreMulti.keys = {
 		const timeCutoff = new Date();
 		timeCutoff.setMinutes(timeCutoff.getMinutes() - 20);
 		if(store.date < timeCutoff) return undefined;
-		return {key: store.key, iv: store.iv};
+		return store.key;
 	},
 	getCyphKey() {
 		var curr = this[this.current];
 		const now = new Date();
-		const timeCutoff = new Date(now.getTime()); 
+		const timeCutoff = new Date(now.getTime());
 		timeCutoff.setMinutes(timeCutoff.getMinutes() - 10);
 		if(curr.date < timeCutoff) {
 			this.current = (this.current === 'A') ? 'B' : 'A';
@@ -29,29 +29,30 @@ EntcoreMulti.keys = {
 			curr.v = (curr.v >= 99) ? 10 : curr.v + 1;
 			curr.date = now;
 			curr.key = crypto.randomBytes(16);
-			curr.iv = crypto.randomBytes(16);
 		}
 		return {
 			keyId: this.current + curr.v,
 			key: curr.key,
-			iv: curr.iv
 		};
 	},
 	seal(value) {
 		const key = this.getCyphKey();
-		const c = crypto.createCipheriv('aes128', key.key, key.iv);
-		var r = c.update(value, 'utf8', 'base64');
-		r += c.final('base64');
-		return key.keyId + r;
+		const iv = crypto.randomBytes(16);
+		const c = crypto.createCipheriv('aes128', key.key, iv);
+		var r = c.update(value, 'utf8');
+		r = Buffer.concat([iv, r, c.final()]);
+
+		return key.keyId + r.toString('base64');
 	},
 	open(value) {
 		const keyId = value.substring(0, 3);
 		const key = this.getDecyphKey(keyId);
 		if(!key) return undefined;
-		const c = crypto.createDecipheriv('aes128', key.key, key.iv);
-		var r = value.substring(3);
-		r = c.update(r, 'base64', 'utf8');
-		r += c.final('utf8');
-		return r;
+		const r = Buffer.from(value.substring(3), 'base64');
+		const iv = r.slice(0, 16);
+		const c = crypto.createDecipheriv('aes128', key, iv);
+		var s = c.update(r.slice(16), 0, 'utf8');
+		s += c.final('utf8');
+		return s;
 	}
 };
